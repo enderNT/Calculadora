69fbd90f060c728353ff7f8186e472e2afbb1f2f
gabriel <gayagocr@gmail.com>
2022-01-18 17:51:32 -0600

    Finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [resultado, setResultado] = useState('')
+
+  const handleClickCalc = (e) => {
+    setResultado(resultado.concat(e.target.name))
+  }
+
+  const handleClickClear = () => {
+    setResultado('')
+  }
+
+  const handleClickC = () => {
+    setResultado(resultado.slice(0, -1))
+  }
+
+  const handleClickCalculate= () => {
+    setResultado(eval(resultado).toString());
+  }
+
+  return(
+  <div className="calculadora">
+        <input className='calculadora__resultado' disabled value={resultado}/>
+        <button className='calculadora__clear' onClick={handleClickClear}>Clear</button>
+        <button className='calculadora__c' onClick={handleClickC}>C</button>
+        <button name='1' onClick={handleClickCalc}>1</button>
+        <button name='2' onClick={handleClickCalc}>2</button>
+        <button name='3' onClick={handleClickCalc}>3</button>
+        <button name ='+' className='calculadora__suma' id='operador' onClick={handleClickCalc}>+</button>
+        <button name='4' onClick={handleClickCalc}>4</button>
+        <button name='5' onClick={handleClickCalc}>5</button>
+        <button name='6' onClick={handleClickCalc}>6</button>
+        <button name='-' className='calculadora__resta' id='operador' onClick={handleClickCalc}>-</button>
+        <button name='7' onClick={handleClickCalc}>7</button>
+        <button name='8' onClick={handleClickCalc}>8</button>
+        <button name='9' onClick={handleClickCalc}>9</button>
+        <button name='*' className='calculadora__multiplicacion' id='operador' onClick={handleClickCalc}>*</button>
+        <button name='0' onClick={handleClickCalc} id='cero'>0</button>
+        <button name='.' className='calculadora__punto' id='operador' onClick={handleClickCalc}>.</button>
+        <button name='/' className='calculadora__division' id='operador' onClick={handleClickCalc}>/</button>
+        <button className='calculadora__igual' id='operador' onClick={handleClickCalculate}>=</button>
     </div>
-  );
+    )
 }
-
 export default App;
